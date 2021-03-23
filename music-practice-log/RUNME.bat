97f8da06f930d4c7a126efb9664713c373f364f4
Samuel Huguet <samhuguet1@gmail.com>
2021-03-23 14:58:51 +0000

    Adding the bat file to run the script quickly

---
+++ music-practice-log/RUNME.bat
@@ -0,0 +1,15 @@
+@echo OFF
+
+rem Define the path to your Anaconda installation.
+set CONDAPATH=C:\ProgramData\Anaconda3
+rem Define here the path to your environment.
+set ENVPATH="C:\Users\Samuel Huguet\.conda\envs\Python3.7Environment"
+
+rem Activate the conda environment
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+
+rem Run a python script in that environment
+python "C:\Users\Samuel Huguet\OneDrive\Documents\Personal\Music-Practice-Log\music-practice-log\main.py"
+
+rem Deactivate the environment
+call conda deactivate
