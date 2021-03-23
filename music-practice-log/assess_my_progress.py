d22a4efcca65975b98312ead77919b889c1e4149
Samuel Huguet <samhuguet1@gmail.com>
2021-03-23 15:31:00 +0000

    New py file to assess progress

---
+++ music-practice-log/assess_my_progress.py
@@ -0,0 +1,6 @@
+from MPL_functions_private import *
+
+## FUNCTION PURPOSE: A function to WhatsApp message the user to remind them to practice their instrument. 
+# Function input arg 1: None. 
+# Function output 1: Log data is added to GitHub, and a WhatsApp message is sent to the user. 
+whatsApp_me()
