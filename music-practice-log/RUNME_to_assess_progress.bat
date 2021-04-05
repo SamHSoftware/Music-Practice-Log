7378dc970342e8bd9dfc1a850d0fced4095af30c
Samuel Huguet <samhuguet1@gmail.com>
2021-04-05 12:17:48 +0100

    Updating bat file in accordance with changes to file names

--- music-practice-log/RUNME_to_assess_progress.bat
+++ music-practice-log/RUNME_to_assess_progress.bat
@@ -1,15 +1,17 @@
 @echo OFF
 
-rem Define the path to your Anaconda installation.
-set CONDAPATH=C:\ProgramData\Anaconda3
+rem Define the path to your Anaconda installation activate.bat file.
+set CONDAPATH=C:\ProgramData\Anaconda3\Scripts\activate.bat
 rem Define here the path to your environment.
 set ENVPATH="C:\Users\Samuel Huguet\.conda\envs\Python3.7Environment"
+rem Define the path to log_my_progress.py.
+set PYFUNCTION="C:\Users\Samuel Huguet\OneDrive\Documents\Personal\Music-Practice-Log\music-practice-log\assess_my_progress.py"
 
 rem Activate the conda environment
-call C:\ProgramData\Anaconda3\Scripts\activate.bat
+call %CONDAPATH% %ENVPATH%
 
 rem Run a python script in that environment
-python "C:\Users\Samuel Huguet\OneDrive\Documents\Personal\Music-Practice-Log\music-practice-log\log_my_progress.py"
+python %PYFUNCTION%
 
 rem Deactivate the environment
 call conda deactivate
