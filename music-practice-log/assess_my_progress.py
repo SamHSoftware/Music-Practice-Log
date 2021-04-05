1f93b2ba0b7cf4d7b6eeb76239b17e568a83e3f0
Samuel Huguet <samhuguet1@gmail.com>
2021-04-05 12:19:25 +0100

    Updated the py file comments and function name and input arg

--- music-practice-log/assess_my_progress.py
+++ music-practice-log/assess_my_progress.py
@@ -1,6 +1,6 @@
-from MPL_functions_private import *
+from MPL_functions import *
 
-## FUNCTION PURPOSE: A function to WhatsApp message the user to remind them to practice their instrument. 
-# Function input arg 1: None. 
+## FUNCTION PURPOSE: A function to WhatsApp or email the user to remind them to practice their instrument or chosen skill. 
+# Function input arg 1: method [string] --> 'email' or 'WhatsApp'. Determins the type of message you recieve. 
 # Function output 1: Log data is added to GitHub, and a WhatsApp message is sent to the user. 
-whatsApp_me()
+message_me(method = 'email')
