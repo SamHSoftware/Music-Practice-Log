d0dfbb6b14436f50e7a7dd2bf5e3992a81f5ca8e
Samuel Huguet <samhuguet1@gmail.com>
2021-04-06 18:40:25 +0100

    Debugging if and elif staements

--- music-practice-log/assess_my_progress.py
+++ music-practice-log/assess_my_progress.py
@@ -3,4 +3,5 @@ from MPL_functions import *
 ## FUNCTION PURPOSE: A function to WhatsApp or email the user to remind them to practice their instrument or chosen skill. 
 # Function input arg 1: method [string] --> 'email' or 'WhatsApp'. Determins the type of message you recieve. 
 # Function output 1: Log data is added to GitHub, and a WhatsApp message is sent to the user. 
-message_me(method = 'email')
+message_me(method = 'email',
+          time_threshold = 1)
