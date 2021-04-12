4e07ef115bdc9e60ab7db242c914ce5ea1a8fe24
Samuel Huguet <samhuguet1@gmail.com>
2021-04-12 18:29:14 +0100

    Modifying code to include more input args

--- music-practice-log/MPL_functions.py
+++ music-practice-log/MPL_functions.py
@@ -179,10 +179,22 @@ def plot_log_data(log_data,
 ## FUNCTION PURPOSE: A function to WhatsApp or email the user to remind them to practice their instrument. 
 # Function input arg 1: method [string] --> 'email' or 'WhatsApp'. Determins the type of message you recieve. 
 # Function input arg 2: time_threshold [int] --> The number of days (discrete value) without practice, after which the user will be sent an e-mail. 
+# Function input arg 3: email_address [string] --> The gmail address you wish to use. 
+# Function input arg 3: email_password [string] --> The 16-digit google app password which you can create online. 
+# Function input arg 4: account_sid [string] --> The application SID if using the Twilio service. 
+# Function input arg 5: authorisation_token [string] --> The authorisation token if using the Twilio service. 
+# Function input arg 6: from_whatsapp_number [string] --> The WhatsApp number you wish to send messages from. 
+# Function input arg 7: to_whatsapp_number [string] --> The WhatsApp number you wish to send messages to. 
 # Function output 1: Log data is added to GitHub, and a message is sent to the user. 
 
 def message_me(method='email', 
-              time_threshold = 1):
+               time_threshold = 1, 
+               email_address = os.environ.get('gmail_address'),
+               email_password = os.environ.get('MPL_gmail_password'), 
+               account_sid = os.environ.get('account_sid'),
+               authorisation_token  = os.environ.get('authorisation_token'), 
+               from_whatsapp_number = os.environ.get('from_whatsapp_number')
+               to_whatsapp_number = os.environ.get('to_whatsapp_number')):
     
     #### (1) Determine whether the user has practiced their instrument recently. 
     # Determine the full file path for our log data.  
@@ -232,12 +244,10 @@ def message_me(method='email',
             except:
                 print('Some error occured while pushing the code')    
         
-            account_sid = os.environ.get('account_sid')
-            authorisation_token  = os.environ.get('authorisation_token')
             client = Client(account_sid, authorisation_token)
 
-            from_whatsapp_number = 'whatsapp:'+os.environ.get('from_whatsapp_number')
-            to_whatsapp_number = 'whatsapp:'+os.environ.get('to_whatsapp_number')
+            from_whatsapp_number = 'whatsapp:'+from_whatsapp_number
+            to_whatsapp_number = 'whatsapp:'+to_whatsapp_number
 
             client.messages.create(body = messages[random_message_idx],
                                   MediaUrl = 'https://github.com/SamHSoftware/Music-Practice-Log/blob/main/log-data/log.png?raw=true',
@@ -247,15 +257,11 @@ def message_me(method='email',
         #### (3b) Send an email.
         elif method == 'email':
         
-            # Fetch the e-mail address and passwords from the system variables. 
-            EMAIL_ADDRESS = os.environ.get('gmail_address')
-            EMAIL_PASSWORD = os.environ.get('MPL_gmail_password')
-
             # Write the e-mail. 
             message = EmailMessage()
             message['Subject'] = 'Practice update'
-            message['From'] = EMAIL_ADDRESS
-            message['To'] = EMAIL_ADDRESS
+            message['From'] = email_address
+            message['To'] = email_address
             main_content = f'{messages[random_message_idx]}\n\nHere is a the graph displaying your practice over time:'
             message.set_content(main_content)
 
@@ -272,5 +278,5 @@ def message_me(method='email',
             
             # Define an SMTP client session object that can be used to send an email.
             with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                smtp.login(email_address, email_password)
                 smtp.send_message(message)
